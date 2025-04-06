b45e4d2cb2d2b9d420286a5d5ed5f4a5b2df349f
Paulo <paulorepecka@gmail.com>
2025-04-06 16:37:08 -0300

    Add files via upload

---
+++ tpfe/index.php
@@ -0,0 +1,65 @@
+<?php include'inc/c.php' ?>
+    <div class="dii1">
+    <img src="img/pzc.jpg" alt="Pizza de Chocolate">
+    <img src="img/pzcl.jpg" alt="Pizza de Calabresa">
+    <img src="img/pzfcc.jpg" alt="Pizza de Frango com Catupiry">
+    <img src="img/pzmgt.jpg" alt="Pizza de Margarita">
+    <img src="img/pzmss.jpg" alt="Pizza de Muçarela">
+    <img src="img/pzn.jpg" alt="Pizza Napolitana">
+    </div>
+    <div class="dii2">
+    <img src="img/pzp.jpg" alt="Pizza de Palmito">
+    <img src="img/pzpp.jpg" alt="Pizza de Peperoni">
+    <img src="img/pzpt.jpg" alt="Pizza Portuguesa">
+    <img src="img/pzq.jpg" alt="Pizza 4 Queijos">
+    <img src="img/pzsp.jpg" alt="Pizza Paulista">
+    <img src="img/pzt.jpg" alt="Pizza Tudo">
+    </div>
+    <div class="d1">
+        <ul class="menu" id="menu">
+            <h1>tipos de pizza</h1>
+            <input id="search" onkeyup="myFunction()" type="text" placeholder="Search.." title="search your order..">
+            <li class="none"><a href="http://localhost/tpfe/csp.php">Paulista</a></li>
+            <li class="none"><a href="http://localhost/tpfe/ct.php">Tudo</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cpt.php">Palmito</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cq.php">4 Queijos</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cpp.php">Peperoni</a></li>
+            <li class="none"><a href="http://localhost/tpfe/ccl.php">Calabresa</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cprt.php">Portuguesa</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cmt.php">Marguerita</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cfcc.php">Frango com catupiry</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cmc.php">Muçarela</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cn.php">Napolitana</a></li>
+            <li class="none"><a href="http://localhost/tpfe/cc.php">Chocolate</a></li>
+    </ul>
+    <div class="d4">
+        <ul>
+            <h1> Temos Diversos tipos de pizza, entre eles:</h1>
+            <li>Paulista:</li>
+                <p>Molho de tomate, muçarela, presunto, palmito, champignon e bacon;   </p>
+            <li>Tudo:</li>
+                <p>junção de todas as pizzas, removendo os sabores de grande contraste;</p>
+            <li>Palmito:</li>
+                <p>Palmito, molho da casa, queijo e presunto;</p>
+            <li>4 Queijos:</li>
+                <p>Queijo muçarela, queijo parmesão, queijo provarole, queijo gorgonzola, molho da casa e presunto;</p>
+            <li>Peperoni:</li>
+                <p>carne bovina, de porco, gordura e temperado com pepperoncino, que é semelhante a pimenta malagueta;</p>
+            <li>Calebresa:</li>
+                <p>molho de tomate, linguiça calabresa defumada e queijo parmesão;</p>
+            <li>Portuguesa:</li>
+                <p>Molho de tomate, queijo muçarela, azeitona, ovo, tomate e presunto;</p>
+            <li>Marguerita:</li>
+                <p>Ovo, molho de tomate, queijo muçarela, tomate e folhas de manjericão;</p>
+            <li>Frango com catupiry:</li>
+                <p>Peito de Frango, molho de tomate, batata palha, queijo parmesão e catupiry;</p>
+            <li>Muçarela:</li>
+                <p>molho de tomate e queijo muçarela;</p>
+            <li>Napolitana:</li>
+                <p>Queijo muçarela, tomate, peperoni e molho de tomate;</p>
+            <li>Chocolate:</li>
+                <p>Cocolate, castanhas e morango;</p>
+        </ul>
+    </div>
+        </div>
+<?php include'inc/r.php' ?>
